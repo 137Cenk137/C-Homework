083c5407456d366d2c117fd94ff33c3f264ad792
Cenk Kıran <cenk33m@gmail.com>
2024-01-23 16:22:10 +0300

    homework2 and homework3 was completed

--- Program.cs
+++ Program.cs
@@ -3,8 +3,9 @@
     private static void Main(string[] args)
     {
         Act act = new Act();
-        act.Homework1();
-        
+        //act.Homework1();
+        //act.Homework2();
+        act.Homework3();
     }
 }
 
@@ -18,7 +19,9 @@ class Act
         {
             Console.Write($"{i+1} . sayiyi giriniz: ");
 
-            ints[i] = Convert.ToInt32(Console.ReadLine()); 
+            ints[i] = Convert.ToInt32(Console.ReadLine());
+            //if(ints[i]%2 == 0)
+             //      console.writeline(ints[i]);       
         }
         foreach (var item in ints)
         {
@@ -26,6 +29,44 @@ class Act
                 Console.WriteLine(item);
 
         }
+
+    }
+
+    public void Homework2() 
+    {
+        Console.WriteLine("2 tane sayı giriniz");
+        Console.Write("first number: ");
+         int n = Convert.ToInt32(Console.ReadLine());
+        Console.Write("second number: ");
+         int m = Convert.ToInt32(Console.ReadLine());
+         int[] ints= new int[n];
+         for (int i = 0; i < n; i++)
+        {
+            Console.Write($"{i+1} . sayiyi giriniz: ");
+
+            ints[i] = Convert.ToInt32(Console.ReadLine());
+            if (ints[i]%m == 0 || ints[i] ==m )
+                Console.WriteLine(ints[i]);
+        }         
+
+    }
+
+    public void Homework3()
+    {
+        Console.Write("bir sayı giriniz: ");
+        Console.Write("first number: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        string[] strings= new string[n];
+         for (int i = 0; i < n; i++)
+        {
+            Console.Write($"{i+1} . kelime giriniz: ");
+
+            strings[i] = Console.ReadLine();
+        }
+       for (int i = n-1; i >=0; i--)
+       {
+            Console.WriteLine(strings[i]);
+       }
 
     }
 }
